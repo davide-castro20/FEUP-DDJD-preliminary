a2a8764dd98f8a7145318306dff6cc655b69028f
davide-castro20 <davideafc@gmail.com>
2022-03-22 21:45:20 +0000

    Added main menu

---
+++ Stationary-Beatriz/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public List<string> scenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(scenes[0]);
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
